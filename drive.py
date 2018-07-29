6ea838ba8fe37124631fb2d27dd5e2e14b9b43d9
mythreya <mythreya.jl@gmail.com>
2018-07-29 00:21:16 -0700

    Modifying drive.py to a value that works properly

--- drive.py
+++ drive.py
@@ -43,8 +43,8 @@ class SimplePIController:
         return self.Kp * self.error + self.Ki * self.integral
 
 
-controller = SimplePIController(0.1, 0.002)
-set_speed = 9
+controller = SimplePIController(0.1, 0.0025)
+set_speed = 10
 controller.set_desired(set_speed)
 
 
